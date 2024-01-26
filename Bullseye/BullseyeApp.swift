60d0b1dc20b087b0f5e4de9bd5dbe8f5cf1c6a58
Guilherme Lourenco <guiramoslou@icloud.com>
2024-01-26 10:28:47 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Guilherme Lourenco on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
